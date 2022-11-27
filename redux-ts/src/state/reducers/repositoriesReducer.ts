f0a61ba71a986a5cfabfe38fb4c6973bd02fc41d
이동건 <developer@idong-geon-ui-MacBookPro.local>
2022-11-27 09:53:27 +0900

    반환유형 타입 추가

---
+++ redux-ts/src/state/reducers/repositoriesReducer.ts
@@ -0,0 +1,23 @@
+interface RepositoriesState {
+  loading: boolean;
+  error: string | null;
+  data: string[];
+}
+
+const reducer = (state: RepositoriesState, action: any): RepositoriesState => {
+  switch (action.type) {
+    case "search_repositories":
+      return { loading: true, error: null, data: [] };
+    case "search_repositories_success":
+      return { loading: false, error: null, data: action.payload };
+    case "search_repositories_error":
+      return { loading: false, error: action.payload, data: [] };
+    default:
+      return state;
+  }
+};
+
+export default reducer;
+
+// 1. 리듀서 매개변수에 state 부분은 인터페이스를 삽입하고 action은 일단 any로 설정해라.
+// 2. switch case문을 작성해라
